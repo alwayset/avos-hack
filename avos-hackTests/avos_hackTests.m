044614a0dd336a23f9794fe9b0d3f449bd7d068f
Eric <tao.china@gmail.com>
2014-03-01 11:36:23 +0800

    1111

---
+++ avos-hackTests/avos_hackTests.m
@@ -0,0 +1,34 @@
+//
+//  avos_hackTests.m
+//  avos-hackTests
+//
+//  Created by Eric Tao on 3/1/14.
+//  Copyright (c) 2014 1xiustudio. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface avos_hackTests : XCTestCase
+
+@end
+
+@implementation avos_hackTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
