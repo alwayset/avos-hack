612a6eba9650f5dea04792ea5d85f2a357112392
Eric <tao.china@gmail.com>
2014-03-01 11:41:56 +0800

    asdasd

---
+++ avos-hack/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  avos-hack
+//
+//  Created by Eric Tao on 3/1/14.
+//  Copyright (c) 2014 1xiustudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
